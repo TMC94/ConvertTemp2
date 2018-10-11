f4385b68976900b61aa9b6632c81b247762026d6
TMC94 <tc818912@ohio.edu>
2018-10-11 18:10:43 -0400

    first commit

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "TempConvert.h"
+
+float convertTemperature(float);
+
+int main()
+{
+    printf("Enter temperature in Celsius: ");
+
+    float tempC;
+    scanf("%f", &tempC);
+
+    float tempF;
+   tempF = convertTemperature(tempC);
+
+    printf("Temperature in Fahrenheit: %f", tempF);
+
+    return 0;
+}
